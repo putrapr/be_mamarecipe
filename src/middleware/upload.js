7369495b9cf984a01542f26b2ca6ab0e25196070
Putra Prasetya <putraprasetya333@gmail.com>
2023-10-08 22:03:24 +0700

    Fix multer "EROFS: read-only file system"

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -5,7 +5,7 @@ const path = require("path");
 const multerUpload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "./public/img_users");
+      cb(null, "/tmp");
     },
     filename: (req, file, cb) => {
       const ext = path.extname(file.originalname);
